f79cd7081d4df0a570e5c903ade3d373ecce9753
Orion Grieco <46628700+Orion-Grieco@users.noreply.github.com>
2024-02-15 19:58:15 -0500

    Update test_2.py

--- gensim_1/test_2.py
+++ gensim_1/test_2.py
@@ -6,12 +6,16 @@ from smart_open import smart_open
 import os
 
 # Creating a corpus
-docs = open(r"PMC001xxxxxx/PMC1193645.xml", "r+")
-tokens = [simple_preprocess(sub_doc) for sub_doc in docs]
-data_dictionary = corpora.Dictionary()
-corpus = [data_dictionary.doc2bow(doc, allow_update = True) for doc in tokens]
-#pprint(corpus)
+corpus = []
+data_folder = "PMC001xxxxxx/"
+for filename in os.listdir(data_folder):
+        with open(data_folder + filename, "r+") as f:
+                sub_doc = f.read()
+                tokens = [simple_preprocess(sub_doc)]
+                data_dictionary = corpora.Dictionary()
+                corpus = [data_dictionary.doc2bow(doc, allow_update = True) for doc in tokens]
+pprint(corpus)
 
 # Creating a dictionary from a text file
-dictionary = corpora.Dictionary(simple_preprocess(line, deacc = True) for line in open(r"PMC001xxxxxx/PMC1193645.xml"))
-print(dictionary.token2id)
+#dictionary = corpora.Dictionary(simple_preprocess(line, deacc = True) for line in open(r"PMC001xxxxxx/PMC1193645.xml"))
+#print(dictionary.token2id)
