5a82248539a71249eb93a02e365ac817fa2ff153
Orion Grieco <46628700+Orion-Grieco@users.noreply.github.com>
2024-02-19 00:58:42 -0500

    rough draft done
    Got a working basic rough draft of text comparison

--- Mouseion/spacy_test.py
+++ Mouseion/spacy_test.py
@@ -18,19 +18,17 @@ file_path = os.path.join(data_folder, filename)
 file = open(file_path, 'r+', encoding = "utf-8")
 file = re.sub(u"[^\x01-\x7f]+",u"",file.read())
 
-
-# creating tokens via nltk:
-#tokens = nltk.word_tokenize(file)
-#tagged = nltk.pos_tag(tokens)
-#entities = nltk.chunk.ne_chunk(tagged)
-
-
-# attempting to create dependency tree:
-#t = treebank.parsed_sents(file)[0]
-#t.draw()
-
-
-introduction_doc = nlp(file)
+'''
+token_file = nlp(file)
+with(open("token_pages.txt", "w+")) as myfile:
+        for token in token_file:
+                myfile.write(f"{token.text}, {token.has_vector}, {token.vector_norm}, {token.is_oov}" + "\n")
+'''
+print(f"File Name and Path : {filename} : {file}")
+comparison_text = input("Enter text to compare to the article: ")
+comparing_text_doc = nlp(comparison_text)
+base_doc = nlp(file)
+print(comparison_text, "<->", filename, base_doc.similarity(comparing_text_doc))
 
 #print(filename)
 #print([token.text for token in introduction_doc])
