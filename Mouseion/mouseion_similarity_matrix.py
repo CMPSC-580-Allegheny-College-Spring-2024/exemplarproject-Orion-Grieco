81c25e123ebc4abd847d47d95c0e797bb580cada
Orion Grieco <46628700+Orion-Grieco@users.noreply.github.com>
2024-02-20 14:56:43 -0500

    updated Mouseion
    Updated to code to iterate through each of the files of the corpus, and evaluate the file via a similarity matrix in comparison/reference to the user input (The user input is unchanged for each file; as to say the same inputted text is the same that is being compared to each individual file that is being compared within the corpus)

---
+++ Mouseion/mouseion_similarity_matrix.py
@@ -0,0 +1,44 @@
+import random
+import spacy #type: ignore
+import nltk
+import nltk.data
+from nltk.corpus import treebank
+import os
+import re
+from bs4 import BeautifulSoup as beau
+nlp = spacy.load("en_core_web_sm")
+
+# Creating stopword list:       
+stopwords = spacy.lang.en.stop_words.STOP_WORDS
+
+data_folder = "PMC001xxxxxx/"
+comparison_text = input("Enter text to compare to the article: ")
+
+for filename in os.scandir(data_folder):
+    #file_path = os.path.join(data_folder, filename)
+
+    open_file = open(filename.path, 'r+', encoding = "utf-8")
+    open_file = re.sub(u"[^\x01-\x7f]+",u"",open_file.read())
+
+    # removing stop words
+    filtered_text = " ".join([word for word in open_file.split() if word not in stopwords])
+
+    soup = beau(filtered_text, "html.parser")
+    for data in soup(['style', 'script']):
+            data.decompose()
+    text = soup.get_text()
+
+    #print(f"File Name and Path : {filename} : {text} + \n")
+    
+    comparing_text_doc = nlp(comparison_text)
+    base_doc = nlp(text)
+    
+    # Creating a dictionary for the similarity matrix
+    similarity_matrix = dict({f'User Input: {comparison_text} + {filename}': base_doc.similarity(comparing_text_doc)})
+    
+    with(open("sim_matrix_results.txt", "a+")) as file:
+        file.write(str(similarity_matrix)+ "\n")
+    #print(comparison_text, "<->", filename, base_doc.similarity(comparing_text_doc))
+
+
+
